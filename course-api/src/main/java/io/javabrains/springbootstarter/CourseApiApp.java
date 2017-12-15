3faae379c5de1b511f7608b4ef68fb0c428a9034
Fernando Fraga <fernandofraga109@gmail.com>
2017-12-14 22:33:29 -0200

    Spring boot demo app

---
+++ course-api/src/main/java/io/javabrains/springbootstarter/CourseApiApp.java
@@ -0,0 +1,17 @@
+package io.javabrains.springbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+
+@SpringBootApplication
+@EnableScheduling
+public class CourseApiApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApiApp.class, args);
+
+	}
+
+}
---
+++ course-api/src/main/java/io/javabrains/springbootstarter/topic/TopicService.java
@@ -0,0 +1,33 @@
+package io.javabrains.springbootstarter.topic;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+
+	private static final Logger log = LoggerFactory.getLogger(TopicService.class);
+
+	private List<Topic> topics = new ArrayList<> (Arrays.asList(
+			new Topic("spring", "Spring Framework", "Spring Framework Description"),
+			new Topic("java", "Core Java", "Core Java description"),
+			new Topic("javascript", "JavaScript", "JavaScript Description")));
+
+	public List<Topic> getAllTopics() {
+		return topics;
+	}
+
+	public Topic getTopic(String id) {
+		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+	
+	public void addTopic(Topic topic) {
+		topics.add(topic);
+	}
+
+}
